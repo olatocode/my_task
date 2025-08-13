9baa1174071d1802f0b8ea78a8ddaf6afd27ec92
tobi <awosolat@gmail.com>
2025-08-13 14:28:01 +0100

    Add task5

---
+++ tasks/task5.py
@@ -0,0 +1,66 @@
+# create and display
+# naija_dish = (
+
+#     input("Enter first dish: "),
+#     input("Enter second dish: "),
+#     input("Enter third dish: ")
+# )
+# print(naija_dish)
+# print("\n".join(naija_dish))
+
+
+# tuple and input
+# friends = (
+#     input("Enter first name: "),
+#     input("Enter second name: "),
+#     input("Enter third name: "),
+#     input("Enter fourth name: "),
+#     input("Enter fifth name: ")
+# )
+
+# print(friends[::-1])
+
+# turple operation
+# states = (
+#      input("Enter first state: "),
+#     input("Enter second state: "),
+#     input("Enter third state: "),
+#     input("Enter fourth state: "),
+#     input("Enter fifth state: ")
+# )
+
+# print("print lagos there: ", "Yes" if "lagos" in states else "No")
+# print("first state", [0])
+# print("last state", [-1])
+
+
+#turple Unpacking
+# profile = (
+#     input("Enter firstname: "),
+#     float(input("Enter age: ")),
+#     input("Enter favourite color: "),
+#     input("Enter home town: ")
+# )
+
+# firstname, age, favorite_color, home_town = profile
+# print(firstname, "\n")
+# print(age, "\n")
+# print(favorite_color, "\n")
+# print(home_town, "\n")
+
+# modify turple indirectly
+shopping_list = (
+    input("Enter first item: "),
+    input("Enter second item: "),
+    input("Enter third item: "),
+)
+
+shopping_list = list(shopping_list)
+shopping_list.append(input("Enter fourth item : "))
+shopping_list.append(input("Enter fifth item : "))
+shopping_list = tuple(shopping_list)
+print("updated shopping list")
+print(" | ".join(shopping_list))
+
+
+# Attendace tracker
